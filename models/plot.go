bff1681be89a95a82dc74c549fcd5419dfda9178
dfaw20 <dfaw200@gmail.com>
2023-09-12 23:39:44 +0900

    modify plot

--- models/plot.go
+++ models/plot.go
@@ -4,34 +4,23 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
-type Event struct {
-	gorm.Model
-	Name string
-}
-
-type PlotEvent struct {
-	gorm.Model
-	PlotID  uint
-	EventID uint
-}
-
 type Support struct {
 	gorm.Model
 	PlotID      uint
 	CharacterID uint
 }
 
+// イベント
 type Plot struct {
 	gorm.Model
-	UserID       uint
-	Title        string
-	Description  string
-	Prompt       string
-	Location     string
-	Season       string
-	Genre        string
-	OutputFormat string
-	ShowWarning  bool
-	Supports     []Event `gorm:"many2many:supports;" json:"-"`
-	Events       []Event `gorm:"many2many:plot_events;" json:"-"`
+	UserID       uint      // 製作者
+	Title        string    // イベントのタイトル
+	Description  string    // イベントを選ぶときに表示される説明
+	Prompt       string    // AIに入力するプロンプト(自由入力)
+	Location     string    // イベントの場所
+	Season       string    // イベントの季節
+	Genre        string    // イベントのジャンル 例:ラブコメ,冒険,ホラーなど
+	OutputFormat string    // 出力形式の指定 例:小説,会話など
+	ShowWarning  bool      // 警告文の出力のフラグ
+	Supports     []Support `gorm:"many2many:supports;" json:"-"` // イベントの登場人物
 }
