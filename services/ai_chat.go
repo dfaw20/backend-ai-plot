bc0a4cd6b49b8125e5df9c943c2c7912e23f586e
dfaw20 <dfaw200@gmail.com>
2023-09-14 22:53:18 +0900

    develop

--- services/ai_chat.go
+++ services/ai_chat.go
@@ -10,8 +10,8 @@ func (g *ApiAiChatGenerator) Generate(prompt string) (string, error) {
 	return "", nil
 }
 
-type LocalSampleGenerator struct{}
+type LocalStubGenerator struct{}
 
-func (g *LocalSampleGenerator) Generate(prompt string) (string, error) {
-	return "", nil
+func (g *LocalStubGenerator) Generate(prompt string) (string, error) {
+	return `The test of a first-rate intelligence is the ability to hold two opposed ideas in mind at the same time and still retain the ability to function.`, nil
 }
